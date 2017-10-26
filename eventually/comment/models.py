3fcc2c90a6d8b226730ff99433440fed1871c9ea
S <masterserg321@gmail.com>
2017-10-26 17:39:37 +0300

    add trial model

--- eventually/comment/models.py
+++ eventually/comment/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from __future__ import unicode_literals
 
+from django.db import models
+from django.utils import timezone
+from django_unixdatetimefield import UnixDateTimeField
 # Create your models here.
+
+class Comment(models.Model):
+    # author = models.ForeignKey('auth.user')
+    text = models.TextField()
+    parent_id = models.IntegerField(blank = True)
+    create_date = models.DateTimeField(
+        default=timezone.now)
+    update_date = models.DateTimeField(
+        default=timezone.now)
+    event_id = models.IntegerField(blank = True)
+    task_id = models.IntegerField(blank = True)
+    vote_id = models.IntegerField(blank = True)
