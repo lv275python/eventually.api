a19c66dadcfbb9771eddf68b36cbf2fcf8c836c9
S <masterserg321@gmail.com>
2017-10-27 16:07:46 +0300

    add create, update, delete, to_dict, get_by_id methods

--- eventually/comment/models.py
+++ eventually/comment/models.py
@@ -2,19 +2,84 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
-# from django_unixdatetimefield import UnixDateTimeField
+from django.utils.dateformat import format
 # Create your models here.
 
 class Comment(models.Model):
-    '''
-    
-    '''
+    """
+        Comment model that describes comment entity.
 
-    text = models.CharField(max_length=1024)
-    create_date = models.DateTimeField(
-        default=timezone.now)
+        The main idea is to document
+        the class with
+
+        :param text: text of comment using CharField with max_length = 1024 symbols
+        :param created_at: date of comment's create using datetime format
+        :param updated_at: date of comment's update using datetime format
+
+    """
+
+    text = models.CharField(max_length = 1024)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
+        """
+        This is a magic method to return string value of model's parametrs in
+         understandible format
+
+        :param self: description
+
+        :return: id, text, create date, update date
+
+        """
+
         return "{} {} {}".format(self.id,
                                  self.text,
-                                 self.create_date)
+                                 self.created_at,
+                                 self.updated_at)
+
+    def get_created_at(self):
+        """
+        This is a method to show date in Unix date format
+
+        :param self: description
+
+        :return: date in Unix date format
+
+        """
+
+        date = format(self.created_at, u'U')
+
+        return date
+
+    def to_dict(self):
+         return {'id': self.id,
+            'text': self.text,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+            }
+
+    @staticmethod
+    def get_by_id(comment_id):
+        try:
+            comment = Comment.objects.get(id=comment_id)
+            return comment
+        except:
+            return None
+
+    @staticmethod
+    def create(text='simple comment'):
+        comment = Comment.objects.create(text)
+        comment.save()
+
+    def update(self, text=None):
+        if text:
+            self.text = text
+
+        self.save()
+
+    @staticmethod
+    def delete_by_id(id):
+        comment = Comment.objects.get(id=comment_id)
+        comment.delete()
+        comment.save()
