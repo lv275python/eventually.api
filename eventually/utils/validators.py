c7f60ba67bdb63de2d492e507ebf86bccc765da7
Dmytro Revak <dmytro.revak@gmail.com>
2017-11-11 21:21:31 +0200

    Add date validator
    Create utils package
    Create validators module
    Added duration validator
    Added timestamp validator

--- eventually/utils/validators.py
+++ eventually/utils/validators.py
@@ -4,6 +4,7 @@ Project validators
 
 Module that provides validation functions for all kinds of project's data.
 """
+import datetime
 
 
 def string_validator(value, min_length=0, max_length=None):
@@ -33,3 +34,43 @@ def string_validator(value, min_length=0, max_length=None):
             return False
 
     return True
+
+
+def duration_validator(value):
+    """
+    Function that provides validation for time duration field.
+
+    :param value: the duration that represented as the amount of second.
+    :type value: int or float
+
+    :return: `True` if value if valid and `None` if it is not.
+    """
+
+    if not isinstance(value, int) and not isinstance(value, float):
+        return
+
+    if value < 0:
+        return
+
+    try:
+        datetime.timedelta(seconds=value)
+        return True
+    except OverflowError:
+        pass
+
+
+def timestamp_validator(value):
+    """
+    Function that provides validation for time date and datetime fields.
+
+    :param value: the timestamp that represented as the amount of second.
+    :type value: int or float
+
+    :return: `True` if value if valid and `None` if it is not.
+    """
+
+    try:
+        datetime.datetime.fromtimestamp(value)
+        return True
+    except (OverflowError, ValueError, OSError, TypeError):
+        pass
