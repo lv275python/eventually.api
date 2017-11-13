499dfa7159a45f38a059b22fc782a5e762579a61
Staryj812 <tteplyj@gmail.com>
2017-11-13 15:39:09 +0200

    HOTFIX: login_user method returns status 400, not 403

--- eventually/authentication/views.py
+++ eventually/authentication/views.py
@@ -3,7 +3,10 @@ Views module
 ============
 """
 
+from utils.utils import json_loads
 from django.views.generic.base import View
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
 
 
 class UserView(View):
@@ -12,3 +15,33 @@ class UserView(View):
 
 def registration_user():
     """Registration CustomUser"""
+
+def login_user(request):
+    """
+    Login of the existing user. Handles post and get requests.
+
+    :param request: request from the website
+    :return: status 302 if login was successful, status 401 if unsuccessful
+    """
+
+    if request.method == "POST":
+        data = json_loads(request.body)
+        if data:
+            user = authenticate(email=data['email'], password=data['password'])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponse(status=200)
+    return HttpResponse(status=400)
+
+
+def logout_user(request):
+    """
+    Logout of the existing user. Handles post and get requests.
+    :param request: request from the website
+    :return: status 200
+    """
+
+    if request.method == "POST":
+        logout(request)
+        return HttpResponse(status=200)
+    return HttpResponse(status=400)
