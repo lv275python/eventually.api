d99262bfe51e4d1da524747ba75fce4167ad52c2
Dmytro Revak <dmytro.revak@gmail.com>
2017-11-13 22:24:04 +0200

    Add coverage verification
    Updated .travis.yml file
    Changed project settings file
    Added .coveragerc file for coverage

--- eventually/eventually/settings.py
+++ eventually/eventually/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'eventually.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
+        'NAME': 'travis_db',
+        'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
