bc0b148e0c0b3d9fff5a8d739ccfce4d866abd06
Dmytro Revak <dmytro.revak@gmail.com>
2017-11-15 02:49:33 +0200

    Added login required middleware
    Created middlewares package
    Create LoginRequiredMiddleware class
    Updated settings file

--- eventually/eventually/settings.py
+++ eventually/eventually/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middlewares.loginrequired.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'eventually.urls'
