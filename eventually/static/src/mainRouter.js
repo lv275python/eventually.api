73f8110dbe30fea9ae70ab6b3ec2dc7b12513aee
JjuliaP <juli.paviv@gmail.com>
2017-12-06 15:31:50 +0200

    Create Curriculum component
    Create Curriculum ui
    Create route path to curriculum page
    Add TopicList to Curriculum component

--- eventually/static/src/mainRouter.js
+++ eventually/static/src/mainRouter.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import Home from './containers/Home';
 import Profile from './containers/profile/Profile';
-import Item from './containers/item/Item'
-import TopicList from './containers/topicList/TopicList'
+import Item from './containers/item/Item';
+import TopicList from './containers/topicList/TopicList';
+import Curriculum from './containers/curriculum/Curriculum';
 
 export default class MainRouter extends React.Component {
     render() {
@@ -11,9 +12,9 @@ export default class MainRouter extends React.Component {
             <main>
                 <Switch>
                   <Route path='/home' component={Home} />
-                  <Route path='/profile' component={Profile} />
+                  <Route path='/profile' component={Profile}/>
+                  <Route path='/curriculum' component={Curriculum}/>
                   <Route path='/item/:itemId' component={Item} />
-                  <Route path='/topiclist' component={TopicList} />                 
                   <Redirect path="*" to="/home" />
                 </Switch>
             </main>
