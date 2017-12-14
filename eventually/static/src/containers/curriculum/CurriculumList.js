ba6c892be52f4edac1dbe43138b97ec1c75a15c2
Silent777 <silentismyfriend@gmail.com>
2017-12-14 15:09:19 +0200

    Topic/Curriculum components

---
+++ eventually/static/src/containers/curriculum/CurriculumList.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { getCurriculums } from './CurriculumService';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import { Link } from 'react-router';
+import { withRouter } from 'react-router-dom';
+import CurriculumLink from './CurriculumLink';
+import CurriculumDialog from './CurriculumDialog';
+
+const containerStyle = {
+    width: '80%',
+    margin: '0 auto',
+};
+
+const CurriculumDialogStyle = {
+    position: 'fixed',
+    right: '3%',
+    top: '85%'
+};
+
+
+class CurriculumList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            curriculums: []
+        };
+
+    }
+
+    componentWillMount() {
+        this.setState({
+            curriculums: getCurriculums()
+        });
+    }
+
+
+    render() {
+        return (
+            <div style={containerStyle}>
+                {
+                    this.state.curriculums.map(curriculum => (
+                        <CurriculumLink
+                            key={curriculum.id.toString()}
+                            title={curriculum.name}
+                            description={curriculum.description}
+                            id={curriculum.id}
+                        />)
+                    )
+                }
+                <CurriculumDialog style={CurriculumDialogStyle}/>
+            </div>
+        );
+    }
+} 
+export default CurriculumList;
