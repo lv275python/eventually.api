0ce57e8a698e3ff7581cb68d13d2df86e795f717
Staryj812 <tteplyj@gmail.com>
2017-12-18 00:05:55 +0200

    Refactored amazons3 view Moved body of post and delete requests to awss3_util. Added tests for amazons3 view

--- eventually/amazons3/views.py
+++ eventually/amazons3/views.py
@@ -3,14 +3,13 @@ Views module
 ============
 """
 
-import json
-import uuid
 import boto3
 from eventually import settings
-from botocore.exceptions import ClientError
 from django.views.generic.base import View
-from django.http import HttpResponse, JsonResponse
-from utils.validators import image_validator
+from django.http import JsonResponse
+from utils.responsehelper import (RESPONSE_400_INVALID_DATA,
+                                  RESPONSE_200_DELETED)
+from utils import awss3_helper
 
 BOTO_S3 = boto3.resource('s3',
                          aws_access_key_id=settings.AWS_S3_ACCESS_KEY_ID,
@@ -22,9 +21,11 @@ class ImageManagement(View):
 
     def post(self, request):
         """
-        Handles upload to Amazon S3 bucket eventually-photos.
-        URL for requests: s3.eu-west-2.amazonaws.com/eventually-photos/
+        Handles POST request to Amazon S3 bucket - eventually-photos.
         Accepts the following image extensions: *.gif, *.png, *.jpg, *.jpeg, *.bmp
+
+        URL for GET requests: s3.eu-west-2.amazonaws.com/eventually-photos/<IMAGE KEY>
+        Image key is a 32-symbol string. E.G.: "088858E477604992B3F5F6BBD478F616"
 
         :param request: request from the web page
         :type request: multipart/form-data
@@ -39,40 +40,19 @@ class ImageManagement(View):
         |   ------WebKitFormBoundary7MA4YWxkTrZu0gW--
 
         :return: Status 200 and JsonResponse with random image name
-                 Status 400 if bad request
-                 Status 409 if image already exists
+                 Status 400 if bad request or image with such name already exists
         """
 
-        if request.content_type != 'multipart/form-data':
-            return HttpResponse(status=400)
+        image_key = awss3_helper.upload(request)
+        if not image_key:
+            return RESPONSE_400_INVALID_DATA
+        return JsonResponse(image_key, status=200)
 
-        image_for_upload = request.FILES.get('image')
-        if not image_for_upload:
-            return HttpResponse(status=400)
-
-        image_extension = image_validator(image_for_upload)
-        if not image_validator(image_for_upload):
-            return HttpResponse(status=400)
-        else: mime_type = 'image/' + image_extension
-
-        image_acl = "public-read"
-        image_key = str(uuid.uuid4()).replace('-', '').upper()
-
-        try:
-            BOTO_S3.Object(settings.AWS_STORAGE_BUCKET_NAME, image_key).load()
-        except ClientError:
-            response = BUCKET.put_object(ACL=image_acl,
-                                         Body=image_for_upload.read(),
-                                         ContentType=mime_type,
-                                         Key=image_key)
-            return JsonResponse({"image_key": response.key}, status=200)
-        else:
-            return HttpResponse(status=409)
 
 
     def delete(self, request):
         """
-        Handles delete image from bucket request
+        Handles DELETE request to Amazon S3 bucket - eventually-photos.
 
         :param request: request from the website
         :type request: application/json
@@ -80,20 +60,7 @@ class ImageManagement(View):
         :return: status 200 if object has been deleted, otherwise - status 400
         """
 
-        if request.content_type != 'application/json':
-            return HttpResponse(status=400)
-
-        image_to_delete = json.loads(request.body)
-        if not image_to_delete.get("image_key"):
-            return HttpResponse(status=400)
-
-        delete_obj = {"Key": image_to_delete.get("image_key")}
-        BUCKET.delete_objects(Delete={"Objects":[delete_obj]})
-
-        try:
-            BOTO_S3.Object(settings.AWS_STORAGE_BUCKET_NAME,
-                           image_to_delete.get("image_key")).load()
-        except ClientError:
-            return HttpResponse(status=200)
-
-        return HttpResponse(status=400)
+        deleted = awss3_helper.delete(request)
+        if not deleted:
+            return RESPONSE_400_INVALID_DATA
+        return RESPONSE_200_DELETED
