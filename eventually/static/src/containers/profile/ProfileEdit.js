7aa6e58ec445476141ee9fb0c63004740cd02428
IvanAndrukh <ivanandrukh@gmail.com>
2017-12-27 11:58:58 +0200

    change profile component

--- eventually/static/src/containers/profile/ProfileEdit.js
+++ eventually/static/src/containers/profile/ProfileEdit.js
@@ -3,52 +3,131 @@ import {putProfileService} from './ProfileService.js';
 
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton'
+import DatePicker from 'material-ui/DatePicker';
+
 
 
 export default class ProfileEdit extends React.Component {
     constructor(props) {
-        super(props);;
-        this.state = {};
-    };
-    
-    handleHobby = event => {
-        this.setState({hobby: event.target.value});
+        super(props);
+        this.state = {
+            first_name: this.props.first_name,
+            middle_name: this.props.middle_name,
+            last_name: this.props.last_name,
+            first_hobby: this.props.hobby,
+            birthday: this.props.birthday,
 
-    }
+        };
+    };
 
     handleFirstName = event => {
         this.setState({first_name: event.target.value});
 
     }
+
+    handleMiddleName = event => {
+        this.setState({middle_name: event.target.value});
+
+    }
+
+    handleLastName = event => {
+        this.setState({last_name: event.target.value});
+
+    }
+
+    handleHobby = event => {
+        this.setState({hobby: event.target.value});
+
+    }
+
+    handleBirthday = (event, date) => {
+        this.setState({birthday: date});
+    }
+    profileEdit = () => {
+        const first_name = this.state.first_name;
+        const middle_name = this.state.middle_name;
+        const last_name = this.state.last_name;
+        const hobby = this.state.hobby;
+        const birthday = this.state.birthday;
+        putProfileService(first_name, middle_name, last_name, hobby,birthday)
+        .then(this.props.getProfile());
+    }
     
     handleSave = event => {
+        const first_name = this.state.first_name;
+        const middle_name = this.state.middle_name;
+        const last_name = this.state.last_name;
         const hobby = this.state.hobby;
-        const first_name = this.state.first_name
-        putUserService(first_name)
-            .then((response) => {
-                this.props.history.push('/profile');
-            });
-        putProfileService(hobby)
-            .then((response) => {
-                this.props.history.push('/profile');
-            });
-        event.preventDefault();
+        const birthday = this.state.birthday;
+        putProfileService(first_name, middle_name, last_name, hobby,birthday)
+        .then(response => {
+            this.props.updateItem(first_name, middle_name, last_name, hobby,birthday);
+            this.props.handleClose();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     };
 
+    
     render() {
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.props.handleClose}
+            />,
+            <FlatButton
+                label="Save"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.handleSave}
+            />,
+        ];
         return (
             <div>
                 <h2>Edit Profile</h2>
                 <TextField
-                    floatingLabelText="Hobby:"
-                    onChange={this.handleHobby}
-                    hintText='Hobby'
-                /><br/>
-                <TextField
-                    floatingLabelText="Name:"
+                    floatingLabelText="First Name:"
                     onChange={this.handleFirstName}
                     hintText='First Name'
-                /><br/>
+                    defaultValue={this.state.first_name}
+                    fullWidth={true}
+                />
+                <TextField
+                    floatingLabelText="Middle Name:"
+                    onChange={this.handleMiddleName}
+                    hintText='Middle Name'
+                    defaultValue={this.state.middle_name}
+                    fullWidth={true}
+                />
+                <TextField
+                    floatingLabelText="Last Name:"
+                    onChange={this.handleLastName}
+                    hintText='Last Name'
+                    defaultValue={this.state.last_name}
+                    fullWidth={true}
+
+                />
+                <TextField
+                    floatingLabelText="Hobby:"
+                    hintText='Hobby'
+                    onChange={this.handleHobby}
+                    fullWidth={true}
+                    multiLine={true}
+                    rowsMax={3}
+                    defaultValue={this.state.hobby}
+                />
+                <DatePicker 
+                    hintText="Birthday"
+                    Dialog mode="landscape" 
+                    openToYearSelection={true}
+                    fullWidth={true}
+                    onChange={this.handleBirthday}
+                    defaultValue={this.state.birthday}
+
+                />
+                
                 <FlatButton label="Save changes" onClick={this.handleSave}/>
             </div>
         );
--- eventually/static/src/containers/profile/ProfileService.js
+++ eventually/static/src/containers/profile/ProfileService.js
@@ -2,23 +2,14 @@ import axios from "axios"
 
 
 export const profileURL = '/api/v1/user/user_id/profile/';
-export const userURL = '/api/v1/user/user_id/';
 
-export const putProfileService = (hobby,birthday) => {
+export const putProfileService = (first_name, middle_name, last_name, hobby, photo, birthday) => {
         return axios.put(profileURL, {
-            hobby,
-            birthday,
-        });
-    };
-
-export const userProfileService = (first_name, middle_name, last_name) => {
-        return axios.put(userURL, {
             first_name, 
             middle_name, 
             last_name,
+            hobby,
+            photo,
+            birthday,
         });
     };
-
-export const getProfileService = (user_id) => {
-	return axios.get(profileURL);
-}
