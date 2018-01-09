4df07c98715e04f8303a09dcd7980bbafcadf726
Dmytro Revak <dmytro.revak@gmail.com>
2018-01-09 15:21:33 +0200

    Added POST and GET chat app logic
    Created GET request logic
    Added showMore button
    Created POST request logic
    Update MessageBar, MessgeSender, MessageList components and messgesListService.js file

--- eventually/static/src/containers/messagesBar/MessagesList.js
+++ eventually/static/src/containers/messagesBar/MessagesList.js
@@ -2,19 +2,39 @@ import React from 'react';
 import MessageItem from './MessageItem';
 import MessagesSender from './MessagesSender';
 import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const messagesListStyle = {
     overflowY: 'scroll',
     maxHeight: '70vh'
+};
+const showMoreButton = {
+    display: 'flex',
+    justifyContent: 'center'
 };
 
 export default class MessagesList extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            pageNumber: 1
+        };
     }
 
+    handleShowMoreClick = this.props.onShowMoreClick
+    handleSendClick = this.props.onSendClick;
+
     render() {
+        const showMoreBtn = this.props.isShowMoreButton ?
+            (<RaisedButton
+                style={showMoreButton}
+                label='Show more'
+                primary={true}
+                onClick={this.handleShowMoreClick} 
+            />) :
+            null;
+
         return (
             <div style={this.props.style}>
                 <Paper zDepth={2} style={messagesListStyle}>
@@ -27,8 +47,9 @@ export default class MessagesList extends React.Component {
                             text={message.text}
                         />)
                     )}
+                    {showMoreBtn}
                 </Paper>
-                <MessagesSender />
+                <MessagesSender receiverId={this.props.receiverId} onSendClick={this.handleSendClick} />
             </div>
         );
     }
--- eventually/static/src/containers/messagesBar/messagesBarService.js
+++ eventually/static/src/containers/messagesBar/messagesBarService.js
@@ -1,4 +1,8 @@
-const getReceiversList = (isMentor=false) => {
+import axios from 'axios';
+
+const appPath = '/api/v1/chat/';
+
+const getReceiversList = (isMentor = false) => {
 
     let receivers = {};
 
@@ -7,21 +11,21 @@ const getReceiversList = (isMentor=false) => {
         receivers = {
             'receivers': [
                 {
-                    'id': 1,
+                    'id': 2,
                     'first_name': 'Sam',
                     'last_name': 'Ruby',
                     'avatar': 'samruby',
                     'is_online': true
                 },
                 {
-                    'id': 2,
+                    'id': 9,
                     'first_name': 'Marcelo',
                     'last_name': 'Petrov',
                     'avatar': 'marcelopetrov',
                     'is_online': false
                 },
                 {
-                    'id': 3,
+                    'id': 10,
                     'first_name': 'Ismail',
                     'last_name': 'Botobo',
                     'avatar': 'ismailbotobo',
@@ -62,144 +66,16 @@ const getReceiversList = (isMentor=false) => {
     return receivers;
 };
 
-const getMessagesList = receiverId => {
-    let messages = null;
-
-    if (receiverId === 1) {
-        messages = {
-            'messages': [
-                {
-                    'id': 1,
-                    'author': 'John Doe',
-                    'avatar': 'johndoe',
-                    'created_at': 1514412290374,
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis. Proin varius lectus nulla, id dictum nisl ultricies eget. Donec vitae gravida justo, ut rhoncus felis. Maecenas mi urna, ornare sed rutrum eu, pulvinar sed lectus. Sed in odio lacinia, maximus arcu at, rhoncus nulla.'
-                },
-                {
-                    'id': 2,
-                    'author': 'Me',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                },
-                {
-                    'id': 3,
-                    'author': 'John Doe',
-                    'avatar': 'johndoe',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis.'
-                },
-                {
-                    'id': 4,
-                    'author': 'Me',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin.'
-                },
-                {
-                    'id': 5,
-                    'author': 'Me',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin.'
-                },
-                {
-                    'id': 6,
-                    'author': 'John Doe',
-                    'avatar': 'johndoe',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis. Proin varius lectus nulla, id dictum nisl ultricies eget. Donec vitae gravida justo, ut rhoncus felis. Maecenas mi urna, ornare sed rutrum eu, pulvinar sed lectus. Sed in odio lacinia, maximus arcu at, rhoncus nulla.'
-                }
-            ]
-        };
-    } else if (receiverId === 2) {
-        messages = {
-            'messages': [
-                {
-                    'id': 1,
-                    'author': 'Eric Moreno',
-                    'avatar': 'ericmoreno',
-                    'created_at': 1514412290374,
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis.'
-                },
-                {
-                    'id': 2,
-                    'author': 'Me',
-                    'created_at': 1514412290374,
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                },
-                {
-                    'id': 3,
-                    'author': 'Eric Moreno',
-                    'avatar': 'ericmoreno',
-                    'created_at': 1514412290374,
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis.Proin varius lectus nulla, id dictum nisl ultricies eget. Donec vitae gravida justo, ut rhoncus felis. Maecenas mi urna, ornare sed rutrum eu, pulvinar sed lectus. Sed in odio lacinia, maximus arcu at, rhoncus nulla'
-                },
-                {
-                    'id': 4,
-                    'author': 'Me',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin.'
-                },
-                {
-                    'id': 5,
-                    'author': 'Me',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin.'
-                },
-                {
-                    'id': 6,
-                    'avatar': 'ericmoreno',
-                    'author': 'Eric Moreno',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis. Proin varius lectus nulla, id dictum nisl ultricies eget. Donec vitae gravida justo, ut rhoncus felis. Maecenas mi urna, ornare sed rutrum eu, pulvinar sed lectus. Sed in odio lacinia, maximus arcu at, rhoncus nulla.'
-                }
-            ]
-        };
-    } else if (receiverId === 3) {
-        messages = {
-            'messages': [
-                {
-                    'id': 1,
-                    'author': 'Mark Smith',
-                    'avatar': 'marksmith',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                },
-                {
-                    'id': 2,
-                    'author': 'Me',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis.'
-                },
-                {
-                    'id': 3,
-                    'author': 'Mark Smith',
-                    'avatar': 'marksmith',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis.'
-                },
-                {
-                    'id': 4,
-                    'author': 'Me',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis.'
-                },
-                {
-                    'id': 5,
-                    'author': 'Me',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin.'
-                },
-                {
-                    'id': 6,
-                    'author': 'Mark Smith',
-                    'avatar': 'marksmith',
-                    'created_at': 1514412290374,                    
-                    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin luctus enim ut sagittis. Proin varius lectus nulla, id dictum nisl ultricies eget. Donec vitae gravida justo, ut rhoncus felis. Maecenas mi urna, ornare sed rutrum eu, pulvinar sed lectus. Sed in odio lacinia, maximus arcu at, rhoncus nulla.'
-                }
-            ]
-        };
-    }
-
-    return messages;
+const getMessagesList = (receiverId, pageNumber) => {
+    const getChatUrl = `${appPath}${receiverId}/${pageNumber}/`;
+    return axios.get(getChatUrl);
 };
 
-export {getReceiversList, getMessagesList};
+const postChatMessage = (receiverId, text) => {
+    const postChatUrl = `${appPath}${receiverId}/`;
+    return axios.post(postChatUrl, {
+        'text': text
+    });
+};
+
+export {getReceiversList, getMessagesList, postChatMessage};
