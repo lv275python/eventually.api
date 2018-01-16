2eb5fb79201f18c277f5998a39280580f612d0d8
Silent777 <silentismyfriend@gmail.com>
2018-01-16 18:47:52 +0200

    added routing

--- eventually/eventually/urls.py
+++ eventually/eventually/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^api/v1/team/', include('team.urls')),
     url(r'^api/v1/img/', include('amazons3.urls')),
     url(r'^api/v1/chat/', include('chat.urls')),
+    url(r'^api/v1/mentor/', include('mentor.urls')),
     url(r'.*', include('home.urls')),
 ]
