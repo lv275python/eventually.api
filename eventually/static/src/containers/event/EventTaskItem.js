4aa5877dda993f578ddbeb63b527544f30d6bc00
Vitalyi Khlivnyi <vitalyi.khlivnyi@gmail.com>
2018-01-22 16:30:25 +0200

    creating Event Task List container add Sortable Decorator add Put and Get Service to container add some style to the container

---
+++ eventually/static/src/containers/event/EventTaskItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+
+export default class EventTaskItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.title,
+            description: this.props.description.slice(0,300)+'...',
+            status: this.props.status
+        };
+    }
+
+    render() {
+        return (
+            <Card>
+                <CardHeader
+                      title={this.state.title}
+                      actAsExpander={true}
+                      showExpandableButton={true}
+                />
+                <CardText expandable={true}>
+                    {this.state.description}
+                </CardText>
+            </Card>
+        );
+    }
+}
+
