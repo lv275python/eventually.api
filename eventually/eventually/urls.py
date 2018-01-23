f09f2addaf147a3c92a78c2f463a01ba0cf729ac
Iryna <iryna.tsuper34@gmail.com>
2018-01-23 13:42:34 +0200

    EventList

--- eventually/eventually/urls.py
+++ eventually/eventually/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^api/v1/team/', include('team.urls')),
     url(r'^api/v1/img/', include('amazons3.urls')),
     url(r'^api/v1/chat/', include('chat.urls')),
+    url(r'^api/v1/events/', EventView.as_view()),
     url(r'^api/v1/mentor/', include('mentor.urls')),
     url(r'^api/v1/events/', EventView.as_view()),
     url(r'.*', include('home.urls')),
