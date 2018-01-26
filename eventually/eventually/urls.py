eb8c543e69b5aa52a0f2bdc79d93a32e7e2617a2
Vitalyi Khlivnyi <vitalyi.khlivnyi@gmail.com>
2018-01-26 18:40:22 +0200

    Create eventTaskList Create sorting for TaskList Add some pure style for list

--- eventually/eventually/urls.py
+++ eventually/eventually/urls.py
@@ -1,6 +1,8 @@
 """Root urls"""
 
 from django.conf.urls import url, include
+from event.views import EventView
+from task.views import TaskView
 
 urlpatterns = [
     url(r'^api/v1/user/', include('authentication.urls')),
@@ -9,5 +11,9 @@ urlpatterns = [
     url(r'^api/v1/img/', include('amazons3.urls')),
     url(r'^api/v1/chat/', include('chat.urls')),
     url(r'^api/v1/mentor/', include('mentor.urls')),
+    url(r'^api/v1/task/(?P<task_id>\d+)/', TaskView.as_view()),
+    url(r'^api/v1/events/(?P<event_id>\d+)/tasks/', TaskView.as_view()),
+    url(r'^api/v1/events/(?P<event_id>\d+)/', EventView.as_view()),
     url(r'.*', include('home.urls')),
 ]
+
