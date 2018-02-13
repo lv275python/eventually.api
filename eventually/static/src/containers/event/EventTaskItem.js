2d4f2a72e3c4e330c0a378489cb736f1f90ecb9d
ChristinaPastushchak <pastushchak98@.com>
2018-02-13 11:02:45 +0200

    add delete button to task

--- eventually/static/src/containers/event/EventTaskItem.js
+++ eventually/static/src/containers/event/EventTaskItem.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import { Card, CardHeader, CardActions, CardText } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import EditEventTaskDialog from './EditEventTaskDialog';
-import { Link } from 'react-router';
-import { withRouter } from 'react-router-dom';
+import {Link} from 'react-router';
 import Task from '../taskItem/Task';
+import {deleteTeamService} from 'src/containers/event/EventService';
 
 
 export default class EventTaskItem extends React.Component {
+    handleDelete = () => {
+        deleteTeamService(this.state.eventId, this.state.id);
+    }
+
     constructor(props) {
         super(props);
         this.state = {
+            id: this.props.id,
             eventId: this.props.eventId,
             title: this.props.title,
-            description: this.props.description.slice(0,300)+'...',
+            description: this.props.description.slice(0, 300) + '...',
             openDialog: false
         };
     }
@@ -30,17 +34,18 @@ export default class EventTaskItem extends React.Component {
                     />
                     <CardActions>
                         <EditEventTaskDialog
-                            title = {this.state.title}
-                            description = {this.state.description}
-                            members = {this.props.members}
-                            id = {this.props.id}
-                            assignmentUsers = {this.props.assignmentUsers}
-                            eventId = {this.props.eventId}
+                            title={this.state.title}
+                            description={this.state.description}
+                            members={this.props.members}
+                            id={this.props.id}
+                            assignmentUsers={this.props.assignmentUsers}
+                            eventId={this.props.eventId}
                         />
                         <Task
-                            eventId = {this.state.eventId}
-                            taskId = {this.props.id}
+                            eventId={this.state.eventId}
+                            taskId={this.props.id}
                         />
+                        <RaisedButton label="Delete" onClick={this.handleDelete}/>
                     </CardActions>
                     <CardText expandable={true}>
                         {this.state.description}
