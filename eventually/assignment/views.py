32f52f6cdeb8f121132af45ab0b01dc6d128f233
YurijH <yurahalysia@gmail.com>
2018-02-16 10:19:20 +0200

    Create graph

--- eventually/assignment/views.py
+++ eventually/assignment/views.py
@@ -1,7 +1,34 @@
 """Views"""
 
 from django.views.generic.base import View
+from django.http import JsonResponse
+from assignment.models import Assignment
+
+from utils.responsehelper import (RESPONSE_404_OBJECT_NOT_FOUND)
 
 
 class AssignmentAnswerView(View):
     """AssignmentAnswer view handles GET, POST, PUT, DELETE requests."""
+    def get(self, request):
+        """
+        Handle GET request.
+
+        :param request: client HttpRequest. Is required
+        :type request: HttpRequest
+
+        :return: return JsonResponse within status and statement data with status 201
+                 or HttpRequest with error if parameters are bad.
+
+        """
+        user = request.user
+        assignments = Assignment.objects.filter(user=user)
+        if assignments:
+            new_date = []
+            for ass in assignments:
+                new_date.append({
+                    'statuses':ass.status,
+                    'statements':ass.statement
+                })
+            data = {'new_date': new_date[::-1]}
+            return JsonResponse(data, status=201)
+        return RESPONSE_404_OBJECT_NOT_FOUND
---
+++ eventually/static/src/containers/graph/ToolTip.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Style } from 'radium';
+
+const toolTipStyles = {
+    '.tooltip': {
+        border: 'solid silver 1px',
+        position: 'fixed',
+        backgroundColor: 'white',
+        borderRadius: '4px',
+        padding: '10px'
+    }
+};
+
+const ToolTip = (props) => (
+    <div className="tooltip-container">
+        <Style scopeSelector=".tooltip-container" rules={toolTipStyles} />
+        <div className="tooltip" style={{ top: props.top, left: props.left }}>
+            {props.children}
+        </div>
+    </div>
+);
+
+// https://github.com/yannickcr/eslint-plugin-react/issues/7
+ToolTip.propTypes = {
+    left: PropTypes.string,
+    top: PropTypes.string,
+    children: PropTypes.node
+};
+
+export default ToolTip;
