69b948ea153dd714e48f73cd38c850f72fe63a98
tarassito <tarassito@gmail.com>
2018-02-22 16:24:46 +0200

    Config html report

--- all-test.js
+++ all-test.js
@@ -1,3 +1,3 @@
-const context = require.context('./eventually/static/src/containers/__tests__/event', true, /.js$/);
+const context = require.context('./eventually/static/src/containers/__tests__', true, /.js$/);
 context.keys().forEach(context);
 module.exports = context;
