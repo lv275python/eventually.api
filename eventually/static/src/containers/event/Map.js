1dc8f5906f56d7bfb4340db364f2975d7196f46c
ChristinaPastushchak <pastushchak98@.com>
2018-02-23 11:59:33 +0200

    change event item style

--- eventually/static/src/containers/event/Map.js
+++ eventually/static/src/containers/event/Map.js
@@ -8,17 +8,16 @@ const MapComponent = compose(
         googleMapURL:
             `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPIKey}
             &language=en&v=3.exp&libraries=geometry,drawing,places`,
-        loadingElement: <div style={{height: '400px'}}/>,
-        containerElement: <div style={{height: '400px'}}/>,
+        loadingElement: <div style={{height: '100%'}}/>,
+        containerElement: <div style={{height: '200px'}}/>,
         mapElement: <div style={{height: '100%'}}/>
     }),
     withScriptjs,
     withGoogleMap
 )(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{lat: props.latitude, lng: props.longitude}}>
+    <GoogleMap defaultZoom={17} defaultCenter={{lat: props.latitude, lng: props.longitude}}>
         <Marker position={{lat: props.latitude, lng: props.longitude}} title={props.name}/>
     </GoogleMap>
 ));
-
 
 export default MapComponent;
