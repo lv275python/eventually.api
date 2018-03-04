c9e4b6c6cba5149084bc2b16af9d3aaf7984edfb
Comrade <dotjurin@gmail.com>
2018-03-05 00:46:51 +0200

    Add delete team button

--- eventually/static/src/containers/teamList/TeamItem.js
+++ eventually/static/src/containers/teamList/TeamItem.js
@@ -2,8 +2,8 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import Subheader from 'material-ui/Subheader';
 import EditTeamDialog from './EditTeamDialog';
-import {getImageUrl} from 'src/helper';
-import {teamServiceGet} from './teamService';
+import {getImageUrl, getUserId} from 'src/helper';
+import {teamServiceGet, teamServiceDelete} from './teamService';
 
 
 const styles = {
@@ -40,11 +40,14 @@ export default class TeamItem extends React.Component {
             name: this.props.name,
             description: this.props.description.slice(0,300)+'...',
             image: this.props.image,
-            members:this.props.members,/*count of members*/
-            listOfMembers:this.props.listOfMembers,/*list with id of members*/
-            listOfNamesMembers:[],
+            members: this.props.members,/*count of members*/
+            listOfMembers: this.props.listOfMembers,/*list with id of members*/
+            listOfNamesMembers: [],
             open: false,
-            open_more:false
+            open_more:false,
+            id: this.props.id,
+            owner: this.props.owner,
+            stateOfTeamView: 0
         };
     }
     /*update old prop*/
@@ -77,6 +80,35 @@ export default class TeamItem extends React.Component {
             });
         }
     }
+    /*check if user is author of team*/
+    ownerCheck = () => {
+        if (getUserId() == this.state.owner){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /*changes state of 'stateOfTeamView' field to display delete team offer */
+    offerDelete = () => {
+        this.setState({stateOfTeamView: 1});
+    }
+    /*changes state of 'stateOfTeamView' field to return to team view*/
+    cancelDelete = () => {
+        this.setState({stateOfTeamView: 0});
+    }
+    /*deletes team and changes state 'stateOfTeamView'*/
+    deleteTeam = () => {
+        teamServiceDelete(this.state.id).then(response => {
+            if (response.status == 200) {
+                this.setState({stateOfTeamView: 2});
+            } else if (response.status == 404) {
+                this.setState({stateOfTeamView: 3});
+            } else {
+                this.setState({stateOfTeamView: 4});
+            }
+        });
+    }
+
     render() {
         styles.container = {
             'background': 'url(' + getImageUrl(this.state.image) + ')',
@@ -93,17 +125,70 @@ export default class TeamItem extends React.Component {
             'marginTop': 37
         };
 
-        return (
-            <div style={styles.container}>
+        let delButton;
+        if (this.ownerCheck()==true) {
+            delButton = <RaisedButton
+                label="Delete" 
+                secondary={true} 
+                style={styles.button}
+                onClick={this.offerDelete}
+            />;
+        } else{
+            delButton = '';
+        }
+
+        let teamView;
+        if (this.state.stateOfTeamView==0) {
+            teamView = (
                 <div style={styles.team}>
                     <Subheader style={styles.header}>{this.state.name}</Subheader>
                     <div style={styles.description}>{this.state.description}</div>
                     <div style={styles.members}>Members:<span>{this.props.members}</span></div>
-                    <RaisedButton label="More info" primary={true} onClick={this.handleToggle} style={styles.button}/>
-                    <RaisedButton label="Edit" onClick={this.handleOpen} style={styles.button}/>
+                    {delButton}
+                    <RaisedButton
+                        label="Edit"
+                        primary={true}
+                        onClick={this.handleOpen}
+                        style={styles.button}
+                    />
+                    <RaisedButton
+                        label="More info"
+                        onClick={this.handleToggle}
+                        style={styles.button}
+                    />
                     <div style={styles.footer}></div>
                 </div>
+            );
+        } else if (this.state.stateOfTeamView==1) {
+            teamView = (
+                <div style={styles.team}>
+                    <p>Are you sure you want to delete team '{this.state.name}'?</p>
+                    <RaisedButton
+                        label="Cancel"
+                        onClick={this.cancelDelete}
+                        primary={true}
+                        style={styles.button}
+                    />
+                    <RaisedButton
+                        label="Delete completely"
+                        onClick={this.deleteTeam}
+                        secondary={true}
+                        style={styles.button}
+                    />
+                    <div style={styles.footer}></div>
+                </div>
+            );
+        } else if (this.state.stateOfTeamView==2) {
+            teamView = <p>Team '{this.state.name}' successfully deleted</p>;
+        } else if (this.state.stateOfTeamView==3) {
+            teamView = <p>Team '{this.state.name}' was not found. Probably, somebody already deleted it. Please, reload page.</p>;
+        } else if (this.state.stateOfTeamView==4) {
+            teamView = <p>Unknown error. Reload page and try again.</p>;
+        }
 
+        return (
+            <div style={styles.container}>
+                {teamView}
                 <EditTeamDialog
                     open = {this.state.open}
                     handleClose = {this.handleClose}
