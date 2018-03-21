edfbc4903670d5d3f0ba1f81684786e6f5ec01b6
Dmytro Tiurin <dtiurtc@training.local>
2018-03-21 17:26:59 +0200

    fix profile navigation

--- eventually/static/src/containers/profile/Profile.js
+++ eventually/static/src/containers/profile/Profile.js
@@ -20,14 +20,22 @@ export default class Profile extends React.Component {
         };
     }
 
+    componentWillReceiveProps(nextProps){
+        if (this.props.match.params.profileId !== nextProps.match.params.profileId) {
+            this.getProfile(+nextProps.match.params.profileId);
+        }
+    }
+
     componentWillMount(){
         this.getProfile(+this.props.match.params.profileId);
     }
 
-    getProfile = (profileId) => {
+
+    getProfile = profileId => {
         getProfileService(profileId).then(response => {
             this.setState({
                 id: response.data['user'],   
+                isMyProfile: profileId === getUserId(),
                 firstName: response.data['first_name'],
                 middleName: response.data['middle_name'],
                 lastName: response.data['last_name'],
